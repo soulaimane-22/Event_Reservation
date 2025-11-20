9a641103e382e8169843df07d1b2d64dea951725
soulaimane-22 <142270040+soulaimane-22@users.noreply.github.com>
2025-11-20 22:14:15 +0100

    intial commit

---
+++ src/test/java/com/event/event_reservation/EventReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.event_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
